144d8b2cfebbfde0fc3b50b2067777d445a8ed71
mrrings <ringisaiedwi@gmail.com>
2025-04-23 20:12:39 +0200

    main terraform code

---
+++ main.tf
@@ -0,0 +1,55 @@
+# main.tf
+
+# Provider configuration
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Resource: VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Resource: Subnet
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+# Resource: Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Resource: Route Table
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+# Resource: Route Table Association
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.route_table.id
+}
